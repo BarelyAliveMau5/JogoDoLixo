3ec3d459ef909af6a6eb470b553e388c0b092df9
BarelyAliveMau5 <theuser@user-PC.this.org>
2016-11-10 03:26:02 -0200

    incluir musica

--- jogo/core/src/com/lixo/Assets.java
+++ jogo/core/src/com/lixo/Assets.java
@@ -1,12 +1,16 @@
 package com.lixo;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 
 public class Assets
 {
 	public static Texture gramado;
+	public static Animation grama;
 	public static Texture fundoMenuPrincipal;
+	public static Music musica;
 	
 	
 	public static Texture carregarTextura(String textura)
@@ -18,5 +22,9 @@ public class Assets
 	{
 		gramado = carregarTextura("gramado.png");
 		fundoMenuPrincipal = carregarTextura("fundo_menu.png");
+		musica = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+		musica.setLooping(true);
+		musica.setVolume(0.5f);
+		musica.play();
 	}
 }
